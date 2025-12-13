1069dc312283689fce9c2ff3b5110f2071a7d424
Théodoros <166630087+tadros12@users.noreply.github.com>
2025-12-13 21:45:39 +0200

    session3

---
+++ src/python/session_3/code/user_info.py
@@ -0,0 +1,14 @@
+def info_user(name,age):
+    """
+        param_1 : take the name from user 
+        type param_1 : str
+        param_2: take age from the user
+        type param_2 :int
+
+
+        return :return string of the name and age
+        type return : str
+    """
+    return f"my name is {name } , my age is {age}"
+
+print(info_user("tad",21))
